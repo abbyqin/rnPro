d5c8e351c2dcaf60251b6e077efe7cd6cddedced
dongxia qin <1213335319@qq.com>
2022-05-10 15:47:56 +0800

    修改首页显示

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import App from './src/pages/HocInstance';
+import App from './src/pages/Home';
 
 AppRegistry.registerComponent(appName, () => App);
